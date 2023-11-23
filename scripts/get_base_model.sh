ea1f71665f7a25f90f484e74ab1653e56e10a45d
-T.K.- <t_k_233@outlook.com>
2023-11-22 23:42:42 -0800

    FIX: fix typo

--- scripts/get_base_model.sh
+++ scripts/get_base_model.sh
@@ -3,4 +3,4 @@ wget -P ./logs/44k/ https://huggingface.co/T-K-233/so-vits-svc-dependencies/reso
 wget -P ./logs/44k/ https://huggingface.co/T-K-233/so-vits-svc-dependencies/resolve/main/v410/D_0.pth
 
 # download the base model for diffusion
-wget -P ./logs/44k/diffusion/ https://huggingface.co/T-K-233/so-vits-svc-dependencies/resolve/main/v410/model_0.pth
+wget -P ./logs/44k/diffusion/ https://huggingface.co/T-K-233/so-vits-svc-dependencies/resolve/main/v410/model_0.pt
